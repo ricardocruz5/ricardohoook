b281f222197d15e00d39a8786759362b93078215
ricardocruz5 <122216331+ricardocruz5@users.noreply.github.com>
2023-01-26 18:20:27 +0000

    Update PrisonLifeGUI.lua

--- PrisonLifeGUI.lua
+++ PrisonLifeGUI.lua
@@ -1,34 +1,40 @@
-local ScreenGui = Instance.new("ScreenGui")
-local CommandBar = Instance.new("Frame")
+local GUI = Instance.new("ScreenGui")
+GUI.Name = "GUI"
+GUI.Parent = game:GetService("CoreGui")
+local Console = Instance.new("Frame")
+local Frame = Instance.new("Frame")
 local List = Instance.new("TextBox")
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-CommandBar.Name = "CommandBar"
-CommandBar.Parent = ScreenGui
-CommandBar.Active = true
-CommandBar.BackgroundColor3 = Color3.fromRGB(32, 32, 32)
-CommandBar.BorderColor3 = Color3.fromRGB(40, 40, 40)
-CommandBar.BorderSizePixel = 2
-CommandBar.Position = UDim2.new(0.764664352, 0, 0.664466441, 0)
-CommandBar.Selectable = true
-CommandBar.Size = UDim2.new(0, 264, 0, 61)
+Console.Name = "Console"
+Console.Parent = GUI
+Console.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+Console.BackgroundTransparency = 0.500
+Console.BorderSizePixel = 0
+Console.Position = UDim2.new(0.5, -217, 1, -70)
+Console.Size = UDim2.new(0, 435, 0, 31)
+Console.Visible = false
+Frame.Parent = Console
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0, 10, 0, 0)
+Frame.Size = UDim2.new(1, -20, 1, 0)
 List.Name = "List"
-List.Parent = CommandBar
-List.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-List.BorderColor3 = Color3.fromRGB(50, 50, 50)
-List.Position = UDim2.new(0, 0, 0.0819672123, 0)
-List.Size = UDim2.new(1, 0, 0, 50)
+List.Parent = Frame
+List.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+List.BackgroundTransparency = 1.000
+List.Size = UDim2.new(1, 0, 1, 0)
+List.ZIndex = 2
 List.ClearTextOnFocus = false
-List.Font = Enum.Font.SourceSans
+List.Font = Enum.Font.SourceSansBold
 List.PlaceholderText = "Press %s to focus."
 List.Text = ""
 List.TextColor3 = Color3.fromRGB(255, 255, 255)
-List.TextSize = 23.000
+List.TextSize = 18.000
+List.TextWrapped = true
 List.TextXAlignment = Enum.TextXAlignment.Left
-
 local Table = {}
 
-for I, V in pairs(ScreenGui:GetDescendants()) do
+for I, V in pairs(GUI:GetDescendants()) do
 	Table[V.Name] = V
 end
 

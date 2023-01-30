db3e81eb5830c367ea2b1e262df414274e9defaf
ricardocruz5 <122216331+ricardocruz5@users.noreply.github.com>
2023-01-30 15:03:40 +0000

    Update PrisonLifeGUI.lua

--- PrisonLifeGUI.lua
+++ PrisonLifeGUI.lua
@@ -1,7 +1,4 @@
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
+--Umm stop looking in Ricarodohook, this is private use!!!1`1
 
 local GUI = Instance.new("ScreenGui")
 local ListTemplate = Instance.new("Frame")
